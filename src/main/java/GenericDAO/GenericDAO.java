720cd5a62bdb30165cf8d6cf2dbe7637f518a857
pedro <pedro.pinguelli@soteica.com.br>
2022-08-23 22:26:05 -0300

    -Create GenericDAO class

---
+++ src/main/java/GenericDAO/GenericDAO.java
@@ -0,0 +1,39 @@
+package GenericDAO;
+
+import java.lang.reflect.ParameterizedType;
+
+import jakarta.persistence.EntityManager;
+
+public class GenericDAO<E, K> {
+	protected Class<E> entity;
+	protected EntityManager manager;
+	
+	@SuppressWarnings("unchecked")
+	public GenericDAO(EntityManager manager) {
+		this.manager = manager;	
+		this.entity = (Class<E>) ((ParameterizedType) this.getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+
+	}
+
+	public void save(E entity) {
+		this.manager.persist(entity);
+	}
+
+	public E getEntity(K key) {
+		return this.manager.find(entity, key);
+	}
+	
+	public void deleteEntity (K key) 
+	{	
+		var deletedEntity= this.getEntity(key);
+		
+		if (deletedEntity != null) 
+		this.manager.remove(deletedEntity);
+		
+		else throw new IllegalArgumentException("Key " + key + " does not exist within entity class " + this.entity.getSimpleName());
+	}
+	
+	
+
+}
