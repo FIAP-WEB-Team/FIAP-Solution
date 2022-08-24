a9299ef3c4dd383357fd5c94d70abaecec8540e2
pedro <pedro.pinguelli@soteica.com.br>
2022-08-23 22:29:22 -0300

    -Create a GenericDAO Class

--- src/main/java/GenericDAO/GenericDAO.java
+++ src/main/java/GenericDAO/GenericDAO.java
@@ -7,13 +7,12 @@ import jakarta.persistence.EntityManager;
 public class GenericDAO<E, K> {
 	protected Class<E> entity;
 	protected EntityManager manager;
-	
+
 	@SuppressWarnings("unchecked")
 	public GenericDAO(EntityManager manager) {
-		this.manager = manager;	
+		this.manager = manager;
 		this.entity = (Class<E>) ((ParameterizedType) this.getClass().getGenericSuperclass())
 				.getActualTypeArguments()[0];
-
 	}
 
 	public void save(E entity) {
@@ -23,17 +22,21 @@ public class GenericDAO<E, K> {
 	public E getEntity(K key) {
 		return this.manager.find(entity, key);
 	}
-	
-	public void deleteEntity (K key) 
-	{	
-		var deletedEntity= this.getEntity(key);
-		
-		if (deletedEntity != null) 
-		this.manager.remove(deletedEntity);
-		
-		else throw new IllegalArgumentException("Key " + key + " does not exist within entity class " + this.entity.getSimpleName());
+
+	public void deleteEntity(K key) {
+		var deletedEntity = this.getEntity(key);
+
+		if (deletedEntity != null)
+			this.manager.remove(deletedEntity);
+
+		else
+			throw new IllegalArgumentException(
+					"Key " + key + " does not exist within entity class " + this.entity.getSimpleName());
 	}
-	
-	
+
+	public void update(K key, E entity) {
+		this.deleteEntity(key);
+		this.save(entity);
+	}
 
 }
